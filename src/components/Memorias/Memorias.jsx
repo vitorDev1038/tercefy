92d812b21835bd16484da60d08c2a4409f3f2499
vitorDev1038 <vitorssouza200@gmail.com>
2026-02-26 23:41:43 -0300

    fix: removendo rastro de localhost e trocando config de nome

--- src/components/Memorias/Memorias.jsx
+++ src/components/Memorias/Memorias.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../../supabaseClient'; // Ajuste o caminho se necessário
+import { supabase } from '../../supabase'; // Ajuste o caminho se necessário
 import './memorias.css';
 
 function Memorias({ onUploadSuccess }) {
